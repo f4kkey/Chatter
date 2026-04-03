63b608825da243ea50381ab7e59ed9e56572a8a2
f4kkey <161198439+f4kkey@users.noreply.github.com>
2026-04-03 11:11:03 +0700

    register

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -1,5 +1,6 @@
-import User from "../models/User";
-
+import { generateToken } from "../lib/util.js";
+import User from "../models/User.js";
+import bcrypt from "bcryptjs";
 export const login = async (req, res) => {
     const { fullName, email, password } = req.body;
 };
@@ -7,18 +8,43 @@ export const login = async (req, res) => {
 export const register = async (req, res) => {
     const { fullName, email, password } = req.body;
     try {
-        if (!fullName || !email || password) {
+        // console.log(fullName, email, password)
+        if (!fullName || !email || !password) {
             return res.status(400).json({ message: "Please provide all required fields" });
         }
 
-        emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+        const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
         if (!emailRegex.test(email)) {
             return res.status(400).json({ message: "Please provide a valid email address" });
         }
 
         const user = User.findOne({ email });
         if (!user) return res.status(400).json({ message: "Email already exists" })
-    } catch (err) {
+        const salt = await bcrypt.genSalt(10)
+        const hashPassword = await bcrypt.hash(password, salt)
 
+        const newUser = new User({
+            fullName,
+            email,
+            password: hashPassword,
+        })
+
+        if (newUser) {
+            generateToken(newUser._id, res)
+            await newUser.save();
+
+            res.status(201).json({
+                _id: newUser._id,
+                email: newUser.email,
+                fullName: newUser.fullName,
+                profilePicture: newUser.profilePicture,
+            })
+        } else {
+            res.status(400).json({ message: "Invalid user data" })
+        }
+
+    } catch (err) {
+        console.log("Error in register controller: ", err)
+        res.status(500).json({ message: "Internal server error" })
     }
 };
--- backend/src/server.js
+++ backend/src/server.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import path from 'path';
+import morgan from 'morgan'
 import { connectDB } from './lib/db.js'
 
 import authRoutes from './routes/auth.route.js';
@@ -10,8 +11,8 @@ dotenv.config();
 const __dirname = path.resolve();
 const PORT = process.env.PORT || 3000;
 const app = express();
-app.use(express.json)
-
+app.use(express.json())
+app.use(morgan("dev"))
 
 app.use('/api/auth', authRoutes);
 app.use('/api/messages', messageRoutes);
