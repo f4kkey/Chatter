47ea87afe1f66ec232627046ea0989d8b8d14de3
f4kkey <161198439+f4kkey@users.noreply.github.com>
2026-04-05 20:54:30 +0700

    online header

--- frontend/src/components/chatContainer/ChatHeader.jsx
+++ frontend/src/components/chatContainer/ChatHeader.jsx
@@ -1,9 +1,11 @@
 import React, { useEffect } from 'react'
 import { useChatStore } from '../../store/useChatStore'
 import { XIcon } from 'lucide-react'
+import { useAuthStore } from '../../store/useAuthStore'
 
 function ChatHeader() {
     const { setSelectedUser, selectedUser } = useChatStore()
+    const { onlineUsers } = useAuthStore()
 
     useEffect(() => {
         const handleEscKey = (e) => {
@@ -17,14 +19,14 @@ function ChatHeader() {
     return (
         <div className='flex justify-between items-center bg-slate-800/50 border-b border-slate-700/50 max-h-[84px] px-6 flex-1'>
             <div className='flex items-center space-x-3'>
-                <div className='avatar online'>
+                <div className={`avatar ${onlineUsers.includes(selectedUser._id) ? 'online' : 'offline'}`}>
                     <div className='w-12 rounded-full'>
                         <img src={selectedUser.profilePicture || "/avatar.png"} alt={selectedUser.fullName} />
                     </div>
                 </div>
                 <div>
                     <h3 className='text-slate-200 font-medium'>{selectedUser.fullName}</h3>
-                    <p className='text-slate-400 text-sm'>Online</p>
+                    <p className='text-slate-400 text-sm'>{onlineUsers.includes(selectedUser._id) ? 'Online' : 'Offline'}</p>
                 </div>
             </div>
             <button onClick={() => setSelectedUser(null)}>
