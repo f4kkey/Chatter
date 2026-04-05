b0ac7ca9c88cbced4ef30d078fb7a52673ada658
f4kkey <161198439+f4kkey@users.noreply.github.com>
2026-04-05 13:30:29 +0700

    chat list

--- frontend/src/components/ProfileHeader.jsx
+++ frontend/src/components/ProfileHeader.jsx
@@ -25,46 +25,43 @@ function ProfileHeader() {
 
 
     return (
-        <div className='p-6 border-b border-slate-700/50'>
-            <div className='flex items-center justify-between'>
-
-                <div className='flex items-center gap-3'>
-                    <div className='avatar online'>
-                        <button className='size-14 rounded-full overflow-hidden relative group'
-                            onClick={() => fileInputRef.current.click()}
-                        >
-                            <img src={selectedImg || authUser.profilePicture || "/avatar.png"} alt="User Image"
-                                className='size-full object-cover'
-                            />
-
-                            <div className="absolute inset-0 bg-black/50 opacity-0 group-hover:opacity-100 flex items-center justify-center transition-opacity">
-                                <span className="text-white text-xs">Change</span>
-                            </div>
-                        </button>
-                        <input type="file"
-                            accept='image/*'
-                            ref={fileInputRef}
-                            onChange={handleImageUpload}
-                            className='hidden'
-                        />
-                    </div>
-
-                    <div>
-                        <h3 className='text-slate-200 font-medium text-base max-w-[180px] truncate'>
-                            {authUser.fullName}
-                        </h3>
-
-                        <p className='text-slate-400 text-xs'>Online</p>
-                    </div>
-                </div>
-
-                <div className='flex gap-4 items-center'>
-                    <button className='text-slate-400 hover:text-slate-200 transition'
-                        onClick={logout}
+        <div className='p-6 border-b border-slate-700/50 flex items-center justify-between'>
+            <div className='flex items-center gap-3'>
+                <div className='avatar online'>
+                    <button className='size-14 rounded-full overflow-hidden relative group'
+                        onClick={() => fileInputRef.current.click()}
                     >
-                        <LogOutIcon />
+                        <img src={selectedImg || authUser.profilePicture || "/avatar.png"} alt="User Image"
+                            className='size-full object-cover'
+                        />
+
+                        <div className="absolute inset-0 bg-black/50 opacity-0 group-hover:opacity-100 flex items-center justify-center transition-opacity">
+                            <span className="text-white text-xs">Change</span>
+                        </div>
                     </button>
+                    <input type="file"
+                        accept='image/*'
+                        ref={fileInputRef}
+                        onChange={handleImageUpload}
+                        className='hidden'
+                    />
                 </div>
+
+                <div>
+                    <h3 className='text-slate-200 font-medium text-base max-w-[180px] truncate'>
+                        {authUser.fullName}
+                    </h3>
+
+                    <p className='text-slate-400 text-xs'>Online</p>
+                </div>
+            </div>
+
+            <div className='flex gap-4 items-center'>
+                <button className='text-slate-400 hover:text-slate-200 transition'
+                    onClick={logout}
+                >
+                    <LogOutIcon />
+                </button>
             </div>
         </div>
     )
